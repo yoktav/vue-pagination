0c4d5bed3acf898d521a2719d0004e4799a29274
Yilmaz <44588992+yoktav@users.noreply.github.com>
2021-10-12 14:24:22 +0300

    Integrate v-model to pagination component

--- pagination.vue
+++ pagination.vue
@@ -1,12 +1,7 @@
 <template>
   <ul class="c-pagination">
     <li v-if="hasPreviousPages" class="c-pagination__item">
-      <button
-        type="button"
-        :data-page-number="0 + 1"
-        class="c-pagination__button"
-        @click="handlePageChange({ action: ACTIONS.FIRST })"
-      >
+      <button type="button" :data-page-number="0 + 1" class="c-pagination__button" @click="handlePageChange({ action: ACTIONS.FIRST })">
         &lt;&lt;
       </button>
     </li>
@@ -26,15 +21,11 @@
       <button
         ref="numberButtons"
         type="button"
-        :class="[
-          'c-pagination__button',
-          'js-pagination-number',
-          currentPage + pageNumber - 1 == currentPage ? CLASSES.IS_ACTIVE : null,
-        ]"
-        :data-page-number="currentPage + pageNumber - 1"
+        :class="['c-pagination__button', 'js-pagination-number', value + pageNumber - 1 == value ? CLASSES.IS_ACTIVE : null]"
+        :data-page-number="pageNumber"
         @click="handlePageChange({ element: $event.target })"
       >
-        {{ currentPage + pageNumber - 1 }}
+        {{ pageNumber }}
       </button>
     </li>
 
@@ -73,7 +64,7 @@ export default {
       type: Number,
       default: 5,
     },
-    currentPage: {
+    value: {
       type: Number,
       default: 1,
     },
@@ -105,23 +96,20 @@ export default {
       pageCountArray: [],
 
       visiblePageCounts: 0,
-
       currentPageNumber: 1,
     };
   },
 
   watch: {
-    currentPage() {
-      this.handleVisiblePageCounts(this.currentPage);
-    },
-    totalPageCount() {
-      this.handleVisiblePageCounts(this.currentPage);
+    value() {
+      this.currentPageNumber = this.value;
+      this.handlePageChange({ elementPageNumber: this.value });
     },
   },
 
   mounted() {
-    this.handleVisiblePageCounts(this.currentPage);
-    this.currentPageNumber = this.currentPage;
+    this.handleVisiblePageCounts(this.value);
+    this.currentPageNumber = this.value;
   },
 
   methods: {
@@ -151,10 +139,45 @@ export default {
       }
     },
 
-    handlePageChange(params) {
-      const { element, action } = params;
+    handleClasses(params) {
+      const { element, elementPageNumber } = params;
 
-      let elementPageNumber = null;
+      this.$refs.numberButtons.forEach(currentItem => {
+        currentItem.classList.remove(this.CLASSES.IS_ACTIVE);
+      });
+
+      this.$nextTick(() => {
+        if (element) {
+          element.classList.add(this.CLASSES.IS_ACTIVE);
+        } else {
+          const selectedElement = this.$el.querySelector(`[${this.NODE_DATASETS.PAGE_NUMER}="${elementPageNumber}"].js-pagination-number`);
+          selectedElement.classList.add(this.CLASSES.IS_ACTIVE);
+        }
+      });
+    },
+
+    handleMoreButtons(params) {
+      const { elementPageNumber } = params;
+
+      if (elementPageNumber == this.totalPageCount) {
+        this.hasNextPages = false;
+        this.hasNextPage = false;
+      } else {
+        this.hasNextPages = true;
+        this.hasNextPage = true;
+      }
+
+      if (elementPageNumber == 1) {
+        this.hasPreviousPages = false;
+        this.hasPreviousPage = false;
+      } else {
+        this.hasPreviousPages = true;
+        this.hasPreviousPage = true;
+      }
+    },
+
+    handlePageChange(params) {
+      let { element, action, elementPageNumber } = params;
 
       if (element) {
         elementPageNumber = parseInt(element.getAttribute(this.NODE_DATASETS.PAGE_NUMER), 10);
@@ -166,58 +189,29 @@ export default {
         return;
       }
 
-      if (action === this.ACTIONS.NEXT) {
-        if (elementPageNumber === this.totalPageCount) {
+      if (action == this.ACTIONS.NEXT) {
+        if (elementPageNumber == this.totalPageCount) {
           return;
         }
 
         elementPageNumber += 1;
-      } else if (action === this.ACTIONS.PREVIOUS) {
-        if (elementPageNumber === 1) {
+      } else if (action == this.ACTIONS.PREVIOUS) {
+        if (elementPageNumber == 1) {
           return;
         }
 
         elementPageNumber -= 1;
-      } else if (action === this.ACTIONS.FIRST) {
+      } else if (action == this.ACTIONS.FIRST) {
         elementPageNumber = 1;
-      } else if (action === this.ACTIONS.LAST) {
+      } else if (action == this.ACTIONS.LAST) {
         elementPageNumber = this.totalPageCount;
       }
 
-      if (elementPageNumber === this.totalPageCount) {
-        this.hasNextPages = false;
-        this.hasNextPage = false;
-      } else {
-        this.hasNextPages = true;
-        this.hasNextPage = true;
-      }
-
-      if (elementPageNumber === 1) {
-        this.hasPreviousPages = false;
-        this.hasPreviousPage = false;
-      } else {
-        this.hasPreviousPages = true;
-        this.hasPreviousPage = true;
-      }
-
+      this.handleMoreButtons({ elementPageNumber });
       this.handleVisiblePageCounts(elementPageNumber);
+      this.handleClasses({ element, elementPageNumber });
 
-      this.$refs.numberButtons.forEach(currentItem => {
-        currentItem.classList.remove(this.CLASSES.IS_ACTIVE);
-      });
-
-      this.$nextTick(() => {
-        if (element) {
-          element.classList.add(this.CLASSES.IS_ACTIVE);
-        } else {
-          const element2 = this.$el.querySelector(
-            `[${this.NODE_DATASETS.PAGE_NUMER}="${elementPageNumber}"].js-pagination-number`,
-          );
-          element2.classList.add(this.CLASSES.IS_ACTIVE);
-        }
-      });
-
-      this.$emit('page-changed', elementPageNumber);
+      this.$emit('input', elementPageNumber);
       this.currentPageNumber = elementPageNumber;
     },
   },
